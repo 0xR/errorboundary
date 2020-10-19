9695cf907982e5854eeb5f662f6da192cbe04c4a
Ruben Oostinga <ruben.oostinga@gmail.com>
2020-10-19 13:43:04 +0200

    Add example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {
+  ErrorBoundary as ReactErrorBoundary
+} from "react-error-boundary";
+import { useLocation, LocationProvider, Link, Router } from "@reach/router";
+
+const FallbackComponent = ({ resetErrorBoundary, error }) => {
+  return (
+    <div>
+      <h1>There was an error!</h1>
+      {(error?.message && <p>{error.message}</p>) ??
+      (error && <p>{JSON.stringify(error)}</p>)}
+    </div>
+  );
+};
+
+export const ErrorBoundary = ({ children }) => {
+  const location = useLocation();
+  return (
+    <ReactErrorBoundary
+      FallbackComponent={FallbackComponent}
+      resetKeys={[location]}
+    >
+      {children}
+    </ReactErrorBoundary>
+  );
+};
+
+const ErrorPage = (props) => {
+  throw new Error("error!");
+  return <p>errorpage</p>;
+};
+
+const NoError = (props) => <p>no error</p>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LocationProvider>
+      <p><Link to="/no-error">noerror</Link></p>
+      <p><Link to="/">error</Link></p>
+      <ErrorBoundary>
+        <Router>
+          <ErrorPage path="/" />
+          <NoError path="/no-error" />
+        </Router>
+      </ErrorBoundary>
+    </LocationProvider>
   );
 }
-
 export default App;
